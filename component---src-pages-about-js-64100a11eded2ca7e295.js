280303fcaaf8cabae3d6d6c07abd20fc7886ebba
rexicq <rexicq@gmail.com>
2026-01-05 22:36:01 +0400

    Updates

---
+++ component---src-pages-about-js-64100a11eded2ca7e295.js
@@ -0,0 +1,40 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"3XHS":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),r=a.n(n),l=a("8k0H"),o=a("F5Ag"),c=a("Eg6Q"),i=a("e+rj"),s=a("dI71"),m=a("vOnD"),d=a("9eSz"),p=a.n(d),u=(a("jpbM"),a("bwEH"));const g=m.c.section`
+  grid-column: full-start / full-end;
+  background-color: ${e=>{let{bg:t}=e;return t}};
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(10, 5vw);
+  grid-gap: 1.5rem;
+  padding: 1.5rem;
+  div {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+`,w=m.c.div`
+  grid-row: ${e=>{let{row:t}=e;return t}};
+  grid-column: ${e=>{let{column:t}=e;return t}};
+`;var b=function(e){function t(t){var a;return(a=e.call(this,t)||this).state={clicked:!1},a.position={0:{row:"1 / span 3",column:"1 / span 3"},1:{row:"1 / span 3",column:"4 / span 3"},2:{row:"1 / span 2",column:"7 / -1"},3:{row:"4 / span 4",column:"1 / span 2"},4:{row:"4 / span 4",column:"3 / span 4"},5:{row:"3 / span 1",column:"7 / span 2"},6:{row:"8 / span 3",column:"1 / span 2"},7:{row:"4 / span 2",column:"7 / span 2"},8:{row:"6 / span 5",column:"7 / span 2"},9:{row:"8 / span 3",column:"3 / span 4"}},a}return Object(s.a)(t,e),t.prototype.render=function(){const{imgs:e,bg:t}=this.props,a=this.position;return r.a.createElement(g,{bg:t},e.map((e,t)=>{let{image:n,title:l}=e;return r.a.createElement(w,{row:a[t].row,column:a[t].column},r.a.createElement(u.a,{showModals:()=>this.setState({clicked:t}),hideModals:()=>this.setState({clicked:!1}),opened:this.state.clicked===t,styleProps:{padding:0},button:r.a.createElement(p.a,{fluid:n.childImageSharp.fluid})},r.a.createElement(p.a,{fluid:n.childImageSharp.fluid})))}))},t}(n.Component),h=a("oTwq");a("MnMr");const{DESKTOP:E,TABLET:f}=h.a,k=m.c.div`
+  padding: 5rem;
+  text-align: center;
+  grid-column: center-start / center-end;
+  text-align: initial;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media (max-width: ${E}px) {
+    grid-template-columns: 1fr;
+  }
+  @media (max-width: ${f}px) {
+  grid-column: 1 / -1;
+  }
+
+`,v=Object(m.c)(i.a)`
+  color: black;
+  margin: 2rem 0;
+  font-size: 2rem;
+`,x=Object(m.c)(i.d)`
+  text-align: initial;
+  margin: 2rem auto;
+`;t.default=e=>{let{data:t}=e;return r.a.createElement(l.a,{header:"small"},r.a.createElement(o.a,{description:t.aboutJson.description,pageTitle:t.aboutJson.title}),r.a.createElement(c.a,{bg:t.aboutJson.header,to:"contact/#location"},r.a.createElement(i.c,null,"О Нас")),r.a.createElement(k,null,r.a.createElement(x,{color:"black"},"Узнайте больше про нас"),r.a.createElement("div",null,r.a.createElement(v,null,'УЗИ "Центрум"-медицинский кабинет ,который начал работать с января 2019 года. Приём пациентов ведут:Локошко Андрей Анатольевич,врач УЗД первой категории с 23-летним стажем работы и Локошко Таисия Федоровна,врач гастроэнтеролог высшей категории со стажем работы более 20 лет.'),r.a.createElement(v,null,'В кабинете сделан ремонт современными материалами,оснащен всей необходимой бытовой и медицинской техникой. Обследования проводятся на аппарате японского производства эксперт класса Toshiba Aplio XG. Для безопасности пациентов и сотрудников установлен бактерицидный рециркулятор "Бактосфера",который обеззараживает воздух от бактерий и вирусов.'),r.a.createElement(v,{color:"black"},"Для удобства пациентов санузел расположен в офисе. Уборка в помещении производится 2 раза в день:до и после окончания работы. Для обследования бесплатно предоставляются бахилы, одноразовые простыни и полотенца."),r.a.createElement(v,{color:"black"},'После приема выдаётся заключение,снимки; даются рекомендации и назначения. Уютная обстановка,внимательные и квалифицированные сотрудники ждут Вас в медицинском кабинете УЗИ "Центрум"!'))),r.a.createElement(b,{bg:"#c1c1c1",imgs:t.aboutJson.gallery}))}},jpbM:function(e,t){e.exports=function(e){return e=e||7,Math.random().toString(35).substr(2,e)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-64100a11eded2ca7e295.js.map
---
+++ sw.js
@@ -0,0 +1,119 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-9a4c0329d4140ff4748e.js"
+  },
+  {
+    "url": "styles.d456ff10ff2c69f4cddc.css"
+  },
+  {
+    "url": "styles-f3e9059f8c2a75df70f2.js"
+  },
+  {
+    "url": "framework-70a2455d4a4320e97528.js"
+  },
+  {
+    "url": "app-83a460433ddd4d228916.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-90a262de4b23279c9d57.js"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "f3a0bc19067a8b27237ea6f8ba05e23c"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "c7047792c6f91b88e0d9abc0cd819e92"
+  },
+  {
+    "url": "page-data/app-data.json",
+    "revision": "f90f3894c16973ef1fb9b3ab78aaf8d3"
+  },
+  {
+    "url": "polyfill-f88fa77a70307660e63d.js"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "e086e51df491343d39ca033c1c3cfdd8"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, workbox.strategies.cacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\page-data\/.*\/page-data\.json/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox.strategies.staleWhileRevalidate(), 'GET');
+
+/* global importScripts, workbox, idbKeyval */
+
+importScripts(`idb-keyval-iife.min.js`)
+
+const { NavigationRoute } = workbox.routing
+
+const navigationRoute = new NavigationRoute(async ({ event }) => {
+  let { pathname } = new URL(event.request.url)
+  pathname = pathname.replace(new RegExp(`^`), ``)
+
+  // Check for resources + the app bundle
+  // The latter may not exist if the SW is updating to a new version
+  const resources = await idbKeyval.get(`resources:${pathname}`)
+  if (!resources || !(await caches.match(`/app-83a460433ddd4d228916.js`))) {
+    return await fetch(event.request)
+  }
+
+  for (const resource of resources) {
+    // As soon as we detect a failed resource, fetch the entire page from
+    // network - that way we won't risk being in an inconsistent state with
+    // some parts of the page failing.
+    if (!(await caches.match(resource))) {
+      return await fetch(event.request)
+    }
+  }
+
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  return await caches.match(offlineShell)
+})
+
+workbox.routing.registerRoute(navigationRoute)
+
+const messageApi = {
+  setPathResources(event, { path, resources }) {
+    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
+  },
+
+  clearPathResources(event) {
+    event.waitUntil(idbKeyval.clear())
+  },
+}
+
+self.addEventListener(`message`, event => {
+  const { gatsbyApi } = event.data
+  if (gatsbyApi) messageApi[gatsbyApi](event, event.data)
+})
