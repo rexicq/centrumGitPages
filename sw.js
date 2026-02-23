a8df0c653e1fbe1ea3479ed701697f7e47f434de
rexicq <rexicq@gmail.com>
2026-02-23 21:16:07 +0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0e73240e5ebe5216f220.js"
+    "url": "webpack-runtime-d08be8d936ceecd90455.js"
   },
   {
     "url": "styles.d456ff10ff2c69f4cddc.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ce2efa2d98240d04b1ecfb5a32021a30"
+    "revision": "ff1425d7d7c9b9457e721fe0d7c19512"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1fa786ece25ff950bbdd39cc494e6c46"
+    "revision": "0445380bf0e8500950d34ba704bedcfd"
   },
   {
     "url": "polyfill-f88fa77a70307660e63d.js"
